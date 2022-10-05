be31644f4319d73ee9c9cb6a0930bfd9f92cadaa
charicodazzi <charicodazzi@hotmail.com>
2022-10-05 00:12:36 -0300

    creando constructores

---
+++ Coche.java
@@ -0,0 +1,28 @@
+public class Coche {
+    // Argumentos
+    String color;
+    String fabricante;
+    String modelo;
+    Double peso;
+    Integer velocidad = 0;
+
+    // Constructores
+    public Coche() {
+    }
+
+    public Coche(String color, String fabricante, String modelo, Double peso, Integer velocidad) {
+        this.color = color;
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.peso = peso;
+        this.velocidad = velocidad;
+    }
+
+    // Comportamiento
+    public void acelerar(Integer cantidad) {
+        if (cantidad > 0 && cantidad <= 120) {
+            this.velocidad += cantidad;
+        }
+    }
+
+}
---
+++ CocheMain.java
@@ -0,0 +1,17 @@
+//Creamos un objeto
+
+public class CocheMain {
+    public static void main(String[] args) {
+        String coche = "alfa romeo";
+        // Acá estamos creando un objeto Coche, que va a tener los atributos que
+        // habiamos creado en Coche.java
+        Coche cocheObj = new Coche();
+        Coche cocheObj2 = new Coche("rojo", "honda", "civic", 1430.50, 43);
+
+        cocheObj2.acelerar(50);
+        System.out.println(cocheObj2);
+
+        cocheObj2.peso=1300.20;
+        System.out.println(cocheObj2);
+    }
+}
